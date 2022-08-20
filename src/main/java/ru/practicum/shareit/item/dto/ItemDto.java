a46e99cc02521c2ad363a26e1e7f537b372c6bc0
Nabokov79 <nabokovsg@yandex.ru>
2022-08-20 20:49:36 +0300

    add DTO class for item

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * // TODO .
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.Create;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+
+    private long id;
+    @NotBlank(groups = {Create.class}, message = "name should not be blank")
+    private String name;
+    @NotBlank(groups = {Create.class}, message = "description should not be blank")
+    private String description;
+    @NotNull(groups = {Create.class}, message = "available should not be null")
+    private Boolean available;
+    private long owner;
+    private Long request;
 }
