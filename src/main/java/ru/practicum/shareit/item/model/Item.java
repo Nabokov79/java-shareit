c209a8111eba2efe0967a0119a730359b141206b
Nabokov79 <nabokovsg@yandex.ru>
2022-08-20 20:47:02 +0300

    add model

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * // TODO .
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.requests.model.ItemRequest;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
+
+    private long id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private long owner;
+    private ItemRequest request;
 }
---
+++ src/main/java/ru/practicum/shareit/requests/model/ItemRequestDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.requests.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemRequestDto {
+
+    private long id;
+    private String description;
+    private String requestor;
+    private LocalDate created;
+}
