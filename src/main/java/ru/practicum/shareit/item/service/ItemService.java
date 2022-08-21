bfe40eafdd6025e4ae60b0d4e3a21fef115f1f76
Nabokov79 <nabokovsg@yandex.ru>
2022-08-21 08:12:53 +0300

    add service for item

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -6,9 +6,14 @@ import java.util.List;
 public interface ItemService {
 
     ItemDto createItem(ItemDto itemDto, long userId);
+
     ItemDto updateItem(long userId, long itemId, ItemDto itemDto);
+
     ItemDto getItem(long itemId);
+
     List<ItemDto> getAllItems(Long userId);
+
     void deleteItem(long itemId);
+
     List<ItemDto> searchItem(Long userId, String text);
 }
