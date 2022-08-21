67bbf72fca4ec9eeb84de0d3f1af604d8404737f
Nabokov79 <nabokovsg@yandex.ru>
2022-08-21 13:36:13 +0300

    add mapper for item

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -5,6 +5,7 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.requests.model.ItemRequest;
 
+
 @Component
 public class ItemMapper {
 
