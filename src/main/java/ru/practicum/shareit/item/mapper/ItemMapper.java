9cb34bab71d2a2d59b1507c236d3bb9b523d034f
Nabokov79 <nabokovsg@yandex.ru>
2022-08-21 08:39:04 +0300

    add mapper for item

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -9,7 +9,7 @@ import ru.practicum.shareit.requests.model.ItemRequest;
 public class ItemMapper {
 
     public static ItemDto toItemDto(Item item) {
-        return new ItemDto (item.getId(),
+        return new ItemDto(item.getId(),
                            item.getName(),
                            item.getDescription(),
                            item.getAvailable(),
@@ -18,11 +18,11 @@ public class ItemMapper {
     }
 
     public static Item toItem(ItemDto itemDto) {
-        return new Item (itemDto.getId(),
-                         itemDto.getName(),
-                         itemDto.getDescription(),
-                         itemDto.getAvailable(),
-                         itemDto.getOwner(),
-                         new ItemRequest());
+        return new Item(itemDto.getId(),
+                        itemDto.getName(),
+                        itemDto.getDescription(),
+                        itemDto.getAvailable(),
+                        itemDto.getOwner(),
+                        new ItemRequest());
     }
 }
