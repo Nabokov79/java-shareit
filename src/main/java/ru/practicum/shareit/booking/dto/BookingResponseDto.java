195e537efce1daf5162dae02789ffce5c22872f7
Nabokov79 <nabokovsg@yandex.ru>
2022-09-11 18:40:47 +0300

    add model DTO

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingResponseDto {
+
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String status;
+    private User booker;
+    private Item item;
+}
