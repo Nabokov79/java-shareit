df46701752b379a08b4d336327fda53c36d89127
Nabokov79 <nabokovsg@yandex.ru>
2022-09-11 18:42:51 +0300

    add exception handler

--- src/main/java/ru/practicum/shareit/exeption/BadRequestException.java
+++ src/main/java/ru/practicum/shareit/exeption/BadRequestException.java
@@ -1,9 +1,9 @@
 package ru.practicum.shareit.exeption;
+public class BadRequestException extends RuntimeException {
 
-public class NotFoundException extends RuntimeException {
     private String massage;
 
-    public NotFoundException(String massage) {
+    public BadRequestException(String massage) {
         super(massage);
     }
 
