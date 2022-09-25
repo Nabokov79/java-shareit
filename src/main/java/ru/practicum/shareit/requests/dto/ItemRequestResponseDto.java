da76fe630663a899e8e5cb3ea7f8402db016ac7e
Nabokov79 <nabokovsg@yandex.ru>
2022-09-25 20:13:30 +0300

    add dto class

---
+++ src/main/java/ru/practicum/shareit/requests/dto/ItemRequestResponseDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.requests.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.shareit.item.dto.ItemDto;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemRequestResponseDto {
+
+    private long id;
+    private String description;
+    private LocalDateTime created;
+    private List<ItemDto> items;
+}
