081ee14181d8b3b43a43df987c49b7b51a259a5f
Nabokov79 <nabokovsg@yandex.ru>
2022-09-25 20:14:54 +0300

    add repository

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.booking.model.Booking;
@@ -8,15 +9,13 @@ import java.util.List;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     @Query(value = "select b from Booking b " +
-                   "where b.booker.id = ?1 " +
-                   "order by b.start desc")
-    List<Booking> findBookingByBookerId(long bookerId);
+                   "where b.booker.id = ?1")
+    List<Booking> findBookingByBookerId(long bookerId, Pageable pageable);
 
     @Query(value = "select b from Booking b " +
                    "left join Item i on i.id = b.item.id " +
-                   "where i.owner.id = ?1 " +
-                   "order by b.start desc")
-    List<Booking> findBookingByOwnerId(long ownerId);
+                   "where i.owner.id = ?1")
+    List<Booking> findBookingByOwnerId(long ownerId, Pageable pageable);
 
     List<Booking> findBookingByItemId(long itemId);
 
--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -2,9 +2,12 @@ package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Item;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    List<Item> findAllByOwnerId(long ownerId);
+    List<Item> findAllByOwnerId(long ownerId, Pageable pageable);
+
+    List<Item> findAllByRequestId(long requestId);
 }
