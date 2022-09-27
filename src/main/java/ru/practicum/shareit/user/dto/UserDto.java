45d874ea3803ddf558190efd01a15dd56ec2ac3b
Nabokov79 <nabokovsg@yandex.ru>
2022-09-27 17:13:51 +0300

    add model

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,13 +2,11 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.common.Create;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
 
