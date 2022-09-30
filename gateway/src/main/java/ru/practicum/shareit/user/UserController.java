20bef6f2f09211999d590484d60a4b56ac8f000e
Nabokov79 <nabokovsg@yandex.ru>
2022-09-30 14:42:16 +0300

    add controller user for gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,53 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.common.Create;
+import ru.practicum.shareit.common.Update;
+import ru.practicum.shareit.user.dto.UserDto;
+
+@Controller
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class UserController {
+
+    private final UserClient userClient;
+
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@Validated({Create.class}) @RequestBody UserDto userDto) {
+        log.info("Received request to create users userDto={}", userDto);
+        return userClient.saveUser(userDto);
+    }
+
+    @PatchMapping(value = "/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long userId,
+                                             @Validated({Update.class}) @RequestBody UserDto userDto) {
+        log.info("Received request update user with userId={}, userDto={}", userId, userDto);
+        return userClient.updateUser(userId, userDto);
+    }
+
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<Object> getUser(@PathVariable Long userId) {
+        log.info("Received request to get user with userId={}", userId);
+        return userClient.getUser(userId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        log.info("Received request to get all users");
+        return userClient.getAllUsers();
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long userId) {
+        log.info("Received request to delete user with userId={}", userId);
+        return userClient.deleteUser(userId);
+    }
+}
