09ec18d943254d71f2d1afe319d7d208d9461b68
Nabokov79 <nabokovsg@yandex.ru>
2022-09-30 14:42:36 +0300

    add controller user for server

--- server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -3,20 +3,17 @@ package ru.practicum.shareit.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.common.Update;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
-import ru.practicum.shareit.common.Create;
 import java.util.List;
 
 @RequestMapping(path = "/users")
-@RestController
+@Controller
 public class UserController {
 
     private final UserService service;
-
 
     @Autowired
     public UserController(UserService service) {
@@ -25,14 +22,12 @@ public class UserController {
 
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@Validated({Create.class}) @RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
         return ResponseEntity.ok().body(service.createUser(userDto));
     }
 
     @PatchMapping(value = "/{userId}")
-    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId,
-                                              @Validated({Update.class})
-                                              @RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
         return ResponseEntity.ok().body(service.updateUser(userId, userDto));
     }
 
