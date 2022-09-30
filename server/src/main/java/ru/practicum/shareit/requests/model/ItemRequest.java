dee8077e54068017bdaebf0aa5f6ff4dcfc6423a
Nabokov79 <nabokovsg@yandex.ru>
2022-09-30 14:47:53 +0300

    add model

--- server/src/main/java/ru/practicum/shareit/requests/model/ItemRequest.java
+++ server/src/main/java/ru/practicum/shareit/requests/model/ItemRequest.java
@@ -1,7 +1,11 @@
 package ru.practicum.shareit.requests.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.shareit.user.model.User;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
