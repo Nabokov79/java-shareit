5fd4b7e49b4efde5770365ad293283635fe709c1
Nabokov79 <nabokovsg@yandex.ru>
2022-09-30 14:40:40 +0300

    add exception for server

---
+++ server/src/main/java/ru/practicum/shareit/exeption/Message.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.exeption;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Message {
+
+    private final String error;
+}
