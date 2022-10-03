e2daba4ffc0a1e63b197d5b721d36e614031b752
Nabokov79 <nabokovsg@yandex.ru>
2022-10-03 16:48:10 +0300

    add dto class

--- gateway/src/main/java/ru/practicum/shareit/requests/dto/ItemRequestCreateDto.java
+++ gateway/src/main/java/ru/practicum/shareit/requests/dto/ItemRequestCreateDto.java
@@ -1,6 +1,9 @@
 package ru.practicum.shareit.requests.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotBlank;
 
 @Setter
