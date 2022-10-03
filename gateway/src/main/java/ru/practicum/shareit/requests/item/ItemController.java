1fd2d58431e2d39ce1e987b6831bb20c59125cef
Nabokov79 <nabokovsg@yandex.ru>
2022-10-03 17:14:41 +0300

    add controller class

---
+++ gateway/src/main/java/ru/practicum/shareit/requests/item/ItemController.java
@@ -0,0 +1,68 @@
+package ru.practicum.shareit.requests.item;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.common.Create;
+import ru.practicum.shareit.common.Update;
+import ru.practicum.shareit.requests.item.dto.CommentRequestDto;
+import ru.practicum.shareit.requests.item.dto.ItemDto;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+@Controller
+@RequestMapping("/items")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class ItemController {
+    private  final ItemClient itemClient;
+
+    @PostMapping
+    public ResponseEntity<Object> createItem(@Validated({Create.class}) @RequestBody ItemDto itemDto,
+                                              @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemClient.createItem(itemDto, userId);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public ResponseEntity<Object> createComment(@PathVariable Long itemId,
+                                                @RequestHeader("X-Sharer-User-Id") Long userId,
+                                                @Validated({Create.class}) @RequestBody CommentRequestDto commentDto) {
+        return itemClient.createComment(commentDto, itemId, userId);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ResponseEntity<Object> updateItem(@PathVariable Long itemId,
+                                              @RequestHeader("X-Sharer-User-Id") Long userId,
+                                              @Validated({Update.class}) @RequestBody ItemDto itemDto) {
+        return itemClient.updateItem(userId, itemId, itemDto);
+    }
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<Object> getItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                   @PathVariable Long itemId) {
+        return itemClient.getItem(userId, itemId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllItems(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                              @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") int from,
+                                              @Positive  @RequestParam(name = "size", defaultValue = "20") int size) {
+        return itemClient.getAllItems(from, size, userId);
+    }
+
+    @DeleteMapping(value = "/{itemId}")
+    public ResponseEntity<Object> deleteItem(@PathVariable Long itemId) {
+        return itemClient.deleteItem(itemId);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Object> searchItemByNameAndDesctription(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                                         @RequestParam String text) {
+        return itemClient.searchItemByNameAndDesctription(userId, text);
+    }
+}
