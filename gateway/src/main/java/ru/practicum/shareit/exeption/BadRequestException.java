30b498b0968c1b12542f374265a8709da31f1699
Nabokov79 <nabokovsg@yandex.ru>
2022-10-03 18:07:36 +0300

    add exception

--- gateway/src/main/java/ru/practicum/shareit/exeption/BadRequestException.java
+++ gateway/src/main/java/ru/practicum/shareit/exeption/BadRequestException.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.exeption;
 public class BadRequestException extends RuntimeException {
-
     public BadRequestException(String message) {
         super(message);
     }
