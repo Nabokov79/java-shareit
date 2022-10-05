bdf802fc114a2be22735026047b8650ab15fc245
Nabokov79 <nabokovsg@yandex.ru>
2022-10-05 11:14:28 +0300

    add dto class

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,20 +2,18 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Value;
 import ru.practicum.shareit.common.Create;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
-@Value
 public class UserDto {
 
     long id;
     @NotBlank(groups = {Create.class}, message = "name should not be blank")
-    String name;
+    private String name;
     @NotBlank(groups = {Create.class}, message = "email should not be blank")
     @Email(groups = {Create.class}, message = "email is not correct")
-    String email;
+    private String email;
 }
